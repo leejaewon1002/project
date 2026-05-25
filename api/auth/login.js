9f19553254bf174ade8a134d1bdcee7691e3d2e4
leejaewon1002 <255534686+leejaewon1002@users.noreply.github.com>
2026-05-25 15:26:10 +0900

    fix: API 라우트 Next.js로 변경

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,4 +1,4 @@
-const { authenticateUser, signToken, isSupabaseConfigured } = require("../_lib/auth");
+const { authenticateUser, signToken, isSupabaseConfigured } = require("../../_lib/auth");
 
 module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
--- api/user/me.js
+++ api/user/me.js
@@ -1,4 +1,4 @@
-const { getUserById, requireAuth, updateUser, isSupabaseConfigured } = require("../_lib/auth");
+const { getUserById, requireAuth, updateUser, isSupabaseConfigured } = require("../../_lib/auth");
 
 module.exports = async function handler(req, res) {
   if (!isSupabaseConfigured()) {
